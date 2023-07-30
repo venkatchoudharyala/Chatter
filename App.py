53e98b38718e9dd035ea597adf5e38cd98c6d95f
Venkat Choudhary Ala <128159176+venkatchoudharyala@users.noreply.github.com>
2023-07-30 19:16:30 +0530

    Update App.py

--- App.py
+++ App.py
@@ -59,7 +59,7 @@ def AccountDisplay(SelectedChat):
 		ChatFile = "ChatRooms/" + GetChatFile(SelectedChat)
 		ChatSelect(UserName, ChatFile, SelectedChat)
 	elif len(Account["Chats"]) == 0:
-		st.write("Expand Ur Account's Sidebar & find ur Friends on PINGIT")
+		st.subheader("Expand Ur Account's Sidebar & find ur Friends on PINGIT")
 
 def GetChatFile(SelectedChat):
 	UserName = UserDetails["Name"]
