2e19a258dafea10e292175cb8312be050e170005
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2023-12-06 13:59:38 +0530

    Create AdminPanel.py

---
+++ AdminPanel.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import json
+
+st.set_page_config(initial_sidebar_state = "collapsed")
+
+hide_st_style = """
+		<style>
+		header {visibility: hidden;}
+		footer {visibility: hidden;}
+  		</style>
+  		"""
+
+st.markdown(hide_st_style, unsafe_allow_html = True)
+
+def main():
+  Form = st.form("Login")
+  UserName = Form.text_input("User Name")
+  Path = "UserAcc/" + UserName.strip() + ".ua"
+  if Form.form_submit_button("Submit"):
+	  try:
+		  with open(Path, "r") as File:
+			  UDetails = File.read()
+			  Details = json.loads(UDetails)
+			  st.write(Details)
+	  except FileNotFoundError:
+		  st.write("User Not Found")
+
+if __name__ == "__main__":
+  main()
